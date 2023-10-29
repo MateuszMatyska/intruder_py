6c5bf45f6fbd6d8f19c1b2f12eae6325643d35ea
mateuszm <mateuszm@pop-os.localdomain>
2023-10-29 22:36:24 +0100

    feat: basic intruder without ext

--- intruder.py
+++ intruder.py
@@ -1,5 +1,9 @@
+from scan_urls import scan_possible_urls
+import asyncio
+
 def main():
     print("Intruder")
+    asyncio.run(scan_possible_urls('http://172.17.0.2'))
 
 if __name__ == "__main__":
     main()
--- scan_urls.py
+++ scan_urls.py
@@ -1,6 +1,21 @@
-def scan_possible_urls():
-    word_list = open("common.txt",'r')
+import requests
+import asyncio
+
+async def do_request(req):
+    return requests.get(req)
+
+async def scan_possible_urls(address):
+    words_list = open("common.txt",'r')
 
     
+    for item in words_list:
+        req = f'{address}/{item}'.strip()
+        connection = await do_request(req)
+        if connection.status_code == requests.codes.ok:
+            print(f'{req} : -- Status OK {connection.status_code}')
+        else:
+            print(f'{req} : {connection.status_code}')
 
-    word_list.close()
+
+
+    words_list.close()
